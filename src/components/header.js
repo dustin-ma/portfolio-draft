66eca3356d8756d37743c3c15b5b6f80ccd8e576
Dustin Ma <dustin8362@gmail.com>
2020-11-12 11:45:15 -0500

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className='header-inner'>
+        <div className='logo'>DUSTIN MA</div>
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>about</a>
+            </li>
+            <li>
+              <a href='/'>portfolio</a>
+            </li>
+            <li>
+              <a href='/'>bandcamp</a>
+            </li>
+            <li>
+              <a href='/'>github</a>
+            </li>
+            <li className='btn'>
+              <a href='/'>contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
